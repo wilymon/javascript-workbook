83a39ff5455f7c209097d589c99d89594dc66661
wilymon <wilhem@gmail.com>
2017-10-02 20:50:55 -0500

    function to convert num to string and vice versa added

---
+++ 01week/datatypes.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//logs current date and time
+let displayDate = function (){
+  let date = new Date();
+  console.log(date);
+}
+
+displayDate();
+
+//converts value of x to a string
+let numtoString = function(){
+  let x = 5;
+  let string = x.toString();
+  console.log(string);
+}
+
+numtoString();
+
+let turnt = function(x){
+  let y = parseInt(x);
+  console.log(y);
+}
+
+turnt('9');
